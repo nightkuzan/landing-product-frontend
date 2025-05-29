25597a54f38326d117d0a0c38e13f2af74af7a5a
nightkuzan <aekkarit12@gmail.com>
2025-05-30 02:24:18 +0700

    config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-css-tags": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
